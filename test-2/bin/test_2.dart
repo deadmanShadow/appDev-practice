94b3958db644fa37b441a1c9e967769f31a2f9c0
Abdullah Raihan Shamil <raihanshamil33@gmail.com>
2023-12-18 19:05:27 +0600

    local add

---
+++ test-2/bin/test_2.dart
@@ -0,0 +1,5 @@
+import 'package:test_2/test_2.dart' as test_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${test_2.calculate()}!');
+}
---
+++ test-2/test/test_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_2/test_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
